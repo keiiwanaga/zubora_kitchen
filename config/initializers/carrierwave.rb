12e078583b4708c50d5417f483a7294872af5dc4
keiiwanaga <kei.iwanaga.1263@gmail.com>
2020-02-12 20:45:03 +0900

    コミットメッセージ2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,23 +1,11 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
-
-CarrierWave.configure do |config|
-  if Rails.env.production? || Rails.env.development? # 開発中もs3使う
-    config.storage :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'zuborakitchen'
-    config.asset_host = 'https://s3.amazonaws.com/zuborakitchen'
-    # NOTE: AWS側の設定を変えなくても、この１行の設定でアップロードできた
-    config.fog_public = false # ←コレ
+if Rails.env.production?
+  CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: 'AWS_ACCESS_KEY_ID',
-      aws_secret_access_key: 'AWS_SECRET_ACCESS_KEY',
-      region: 'ap-northeast-1',
-      # path_style: true
+      aws_access_key_id: ENV['ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
+      region: 'ap-northeast-1'# S3バケット作成時に指定したリージョン。左記は東京を指す
     }
-  else
-    config.storage :file
-    config.enable_processing = false if Rails.env.test?
+    config.fog_directory  = 'zuborakitchen' # 作成したS3バケット名
   end
+end
